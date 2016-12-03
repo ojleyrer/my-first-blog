7a928dad4840b719b2937cd8d1c0a6b195e7a046
Olivia Leyrer <olivia.leyrer@gmail.com>
2016-12-03 17:01:03 -0600

    "I made/changed the html template for my blog"

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
+from django.utils import timezone
+from blog import models
 
 def post_list(request):
-	return render(request, 'blog/post_list.html',{})
+	posts = models.Post.objects.all() 
+	d = {"posts": posts}
+	return render(request, 'blog/post_list.html',d) 
 	
+
 
 # Create your views here.
